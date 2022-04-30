8bf65b6cb21ac4ab074ea2bd324cd0feef404d6f
mlgarchery <martial.garchery@gmail.com>
2022-04-30 20:03:23 +0200

    transition back

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import Konva from "konva";
-import { Stage, Layer, Text } from "react-konva";
+import { Stage, Layer, Group, Rect } from "react-konva";
 import Box from "./components/box";
 import Arrow from "./components/arrow";
 import rootBox from "./diagram.json";
-import { useState, useEffect, useTransition } from "react";
+import { useState, useEffect, useTransition, useRef } from "react";
 import NavigationInput from "./components/navigationInput";
 import ArrowTip from "./components/arrowTip";
 
 
 function App() {
   const canvasSize = Math.min(window.innerWidth, window.innerHeight);
+  const currentViewWrapper = useRef();
 
   const [, startTransition] = useTransition();
   const [currentBox, setCurrentBox] = useState(rootBox);
 
   let box_map = {};
   currentBox.boxes.forEach((box) => {
+    box.parent = currentBox;
     box_map[box.id] = box;
   });
   const [boxMap, setBoxMap] = useState(box_map);
 
+  function handleBoxTransitionBack(parentBox){
+    // transition
+    console.log(parentBox);
+    // parentBox.parentCanvas // we will need this later
+
+    currentViewWrapper.current.to({
+      x: parentBox.x ?? canvasSize/2,
+      y: parentBox.y ?? canvasSize/2,
+      scaleX: parentBox.w ?? 0.2,
+      scaleY: parentBox.h ?? 0.2,
+      duration: 0.3,
+      onFinish: () => {
+        console.log("We transitioned back to the parent !")
+        let box_map = {};
+        parentBox.boxes.forEach((b) => {
+            b.parent = parentBox;
+            box_map[b.id] = b;
+          });
+        setBoxMap(box_map);
+        setCurrentArrowTipsMap(buildTips(parentBox.arrows, box_map))
+        setCurrentBox(parentBox);
+        currentViewWrapper.current.to({
+          x: 0,
+          y: 0,
+          scaleX: 1,
+          scaleY: 1,
+          duration: 0,
+        });
+      }
+    })
+  }
+
   function handleBoxTransition(canvasObj, box) {
+    box.parentCanvas = canvasObj;
+
     startTransition(() => {
       canvasObj.moveToTop();
       canvasObj.to({
@@ -28,11 +64,12 @@ function App() {
         y: 0,
         scaleX: canvasSize / box.w,
         scaleY: canvasSize / box.h,
-        duration: 0.35,
+        duration: 0.3,
         easing: Konva.Easings.EaseInOut,
         onFinish: () => {
           let box_map = {};
           box.boxes.forEach((b) => {
+            b.parent = box;
             box_map[b.id] = b;
           });
 
@@ -72,21 +109,23 @@ function App() {
   ));
 
   return (
-    <div>
-      <NavigationInput jsonTree={rootBox} current={currentBox} />
-      <h2>Capt</h2>
+    <div id="app-root">
+      <NavigationInput jsonTree={rootBox} current={currentBox} handleBoxTransitionBack={handleBoxTransitionBack}/>
       <Stage
         width={canvasSize}
         height={canvasSize}
         style={{ backgroundColor: "white", width: canvasSize, height: canvasSize }}
       >
         <Layer>
-          {boxes}
-          {arrows}
-          {Object.entries(currentArrowTipsMap).flatMap(([id, value]) => [
-            <ArrowTip x={value.start.x} y={value.start.y} onMove={(x, y) => setCurrentArrowTipsMap({ ...currentArrowTipsMap, [id]: { ...currentArrowTipsMap[id], start: { x, y } } })} />,
-            <ArrowTip x={value.end.x} y={value.end.y} onMove={(x, y) => setCurrentArrowTipsMap({ ...currentArrowTipsMap, [id]: { ...currentArrowTipsMap[id], end: { x, y } } })} />
-          ])}
+          <Group ref={currentViewWrapper} >
+            <Rect width={canvasSize} height={canvasSize} stroke="black" strokeWidth={10}></Rect>
+            {boxes}
+            {arrows}
+            {Object.entries(currentArrowTipsMap).flatMap(([id, value]) => [
+              <ArrowTip x={value.start.x} y={value.start.y} onMove={(x, y) => setCurrentArrowTipsMap({ ...currentArrowTipsMap, [id]: { ...currentArrowTipsMap[id], start: { x, y } } })} />,
+              <ArrowTip x={value.end.x} y={value.end.y} onMove={(x, y) => setCurrentArrowTipsMap({ ...currentArrowTipsMap, [id]: { ...currentArrowTipsMap[id], end: { x, y } } })} />
+            ])}
+          </Group>
         </Layer>
       </Stage>
     </div>
