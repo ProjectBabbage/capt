95cbda7f19f1b6444587acb63dd795818510d0b5
DemiSel <e.sebbar@gmail.com>
2022-05-06 02:43:29 +0200

    Display editable notes, use markdown format

--- src/components/notes.tsx
+++ src/components/notes.tsx
@@ -1,13 +1,32 @@
 import { Box } from "../types/box";
+import ReactMarkdown from 'react-markdown'
+import { useEffect, useState } from "react";
 
 export default function Notes({box}: {box: Box}){
+    const [editMode, setEditMode] = useState(false);
+    const [editValue, setEditValue] = useState(box.notes ?? '');
+
+    useEffect(() => {
+        setEditValue(box.notes ?? '');
+    }, [box])
+
+    useEffect(() => {
+        box.notes = editValue;
+    }, [editValue])
 
     return(
-        <div className="fc">
+        <div className="fc sidebar_notes full-height flex-grow">
             <h1>{box.text}</h1>
-            <div>
-                {box.notes ?? ''}
-            </div>
+            <button onClick={(evt) => setEditMode(!editMode)}>Edit</button>
+                {!editMode && (
+                <ReactMarkdown>{editValue}</ReactMarkdown>
+                )}
+                {editMode && (
+                    <textarea  
+                    value={editValue}
+                    className="full-width flex-grow"
+                    onChange={(evt) => {setEditValue(evt.target.value)}} ></textarea>
+                )}
         </div>
     )
 }
